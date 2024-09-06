ccace3e1a80e5ba91e2b24e031725a67932e19f7
EngKhallo <engkhalid.ict@gmail.com>
2024-09-06 17:54:34 +0800

    export processed data into CSV file format

--- scripts/etl/transform.py
+++ scripts/etl/transform.py
@@ -19,6 +19,7 @@ print(df.duplicated().sum())
 
 # convert 'updated' column into actual date-time format
 df_cleaned['updated'] = pd.to_datetime(df_cleaned['updated'], unit='ms')
+df_cleaned['updated'] = df_cleaned['updated'].dt.date  # Convert to date only
 print('updated column: ', df_cleaned['updated'])
 
 # Add new column: Case Fatility Rate (CFR) = (Deaths / Cases) * 100
@@ -36,6 +37,14 @@ df_cleaned.rename(columns={
     'deaths': 'total_deaths', 
     'population': 'total_population'
 }, inplace=True)
+
+
+# ------ Save the cleaned and transformed data as dataset and export it ------ #
+df_cleaned.to_csv('data/processed/east_africa_covid_data_transformed.csv', index=False)
+
+print("Transformation complete. Processed data saved to 'data/processed/east_africa_covid_data_transformed.csv'.")
+
+
 
 
 
