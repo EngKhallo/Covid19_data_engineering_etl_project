3838bb283c8c4888b351ad7dc2d4f3bc2b335e99
EngKhallo <engkhalid.ict@gmail.com>
2024-09-10 15:47:43 +0800

    Load metrics data to db

--- scripts/etl/load.py
+++ scripts/etl/load.py
@@ -21,3 +21,25 @@ for index, row in df[['country', 'countryInfo.iso2', 'countryInfo.iso3', 'countr
 
 # Commit changes to the 'countries' table
 conn.commit()
+
+# Insert metrics into the 'metrics' table
+for index, row in df.iterrows():
+    cursor.execute("SELECT country_id FROM countries WHERE country_name = %s", (row['country'],))
+    country_id = cursor.fetchone()[0]
+    
+    cursor.execute("""
+        INSERT INTO metrics (country_id, updated, total_cases, today_cases, total_deaths, today_deaths, recovered, today_recovered, 
+                             active, critical, cases_per_million, deaths_per_million, recovered_per_million, critical_per_million, 
+                             case_fatality_rate, recovered_rate)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, (
+        country_id, row['updated'], row['total_cases'], row['todayCases'], row['total_deaths'], row['todayDeaths'],
+        row['recovered'], row['todayRecovered'], row['active'], row['critical'], row['casesPerOneMillion'], 
+        row['deathsPerOneMillion'], row['recoveredPerOneMillion'], row['criticalPerOneMillion'],
+        row['case_fatility_rate'], row['recovered_rate']  # Update here
+    ))
+
+# Commit changes to the 'metrics' table
+conn.commit()
+
+
