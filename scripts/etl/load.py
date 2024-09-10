6c1d6f41d39918e07c0ca7bed3d3b2ba58440fb7
EngKhallo <engkhalid.ict@gmail.com>
2024-09-10 13:08:19 +0800

    database connection

--- scripts/etl/load.py
+++ scripts/etl/load.py
@@ -1,6 +1,12 @@
 import pandas as pd
-import psycopg2
+import psycopg2 as psy
 
 # Import pre-processed raw dataset
 df = pd.read_csv('data/processed/east_africa_covid_data_transformed.csv')
 
+# Connect to PostgreSQL
+conn = psy.connect(
+    host='localhost', database='covid19_project', user='postgres', passwd='Developer'
+)
+
+cursor = conn.cursor()
