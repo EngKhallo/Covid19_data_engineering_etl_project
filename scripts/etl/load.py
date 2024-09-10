ff3d766f342a29b11783ae903abe779ab78666b8
EngKhallo <engkhalid.ict@gmail.com>
2024-09-10 13:38:33 +0800

    load and insert countries data to the database

--- scripts/etl/load.py
+++ scripts/etl/load.py
@@ -6,7 +6,18 @@ df = pd.read_csv('data/processed/east_africa_covid_data_transformed.csv')
 
 # Connect to PostgreSQL
 conn = psy.connect(
-    host='localhost', database='covid19_project', user='postgres', passwd='Developer'
+    host='localhost', database='covid19_data_pipeline', user='postgres', password='Developer'
 )
 
 cursor = conn.cursor()
+
+# Insert Countries into the countries table
+for index, row in df[['country', 'countryInfo.iso2', 'countryInfo.iso3', 'countryInfo.lat', 'countryInfo.long', 'total_population']].drop_duplicates().iterrows():
+    cursor.execute("""
+        INSERT INTO countries (country_name, iso2, iso3, lat, long, total_population)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        ON CONFLICT (country_name) DO NOTHING;
+    """, (row['country'], row['countryInfo.iso2'], row['countryInfo.iso3'], row['countryInfo.lat'], row['countryInfo.long'], row['total_population']))
+
+# Commit changes to the 'countries' table
+conn.commit()
