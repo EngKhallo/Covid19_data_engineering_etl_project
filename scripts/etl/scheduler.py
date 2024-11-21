bdf0dbcd63dd2ae1868186a26c23811c9acada8e
EngKhallo <engkhalid.ict@gmail.com>
2024-11-21 16:58:22 +0800

    Schedule: for scheduling the etl piepline function in time

---
+++ scripts/etl/scheduler.py
@@ -0,0 +1,20 @@
+import schedule
+import time
+from extract import extract_data
+from transform import transform_data
+from load import load_data
+
+def etl_job():
+   # Extract the raw data
+    extract_data()
+    # Transform the data
+    transform_data()  # Remove the argument here
+    # Load the data into the database
+    load_data()
+
+# Schedule the job to run daily
+schedule.every().day.at("16:49").do(etl_job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
